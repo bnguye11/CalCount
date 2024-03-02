73134a01948dd4180d27da49f3dafaa57e4391c0
Brunhilde <brandontu35@gmail.com>
2024-03-01 19:32:34 -0500

    feat added panel for adding calorie information manually and basic styling for it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:calcount/panel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,12 +8,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cal Counter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Cal Counter'),
     );
   }
 }
@@ -55,18 +56,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final int _counter = 0;
+  final ScrollController controller = ScrollController();
+  final PanelController panelController = PanelController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,49 +69,46 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final panelHeightOpen = MediaQuery.of(context).size.height * 0.8;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SlidingUpPanel(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  panelController.open();
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        controller: panelController,
+        maxHeight: panelHeightOpen,
+        defaultPanelState: PanelState.CLOSED,
+        minHeight: 0,
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+        backdropTapClosesPanel: true,
+        //panel: ,
+        panelBuilder: (controller) {
+          return Panel(
+            controller: controller,
+            panelController: panelController,
+          );
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
---
+++ lib/panel.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+final foodName = TextEditingController();
+final calories = TextEditingController();
+
+//macros
+final carbs = TextEditingController();
+final protien = TextEditingController();
+final fats = TextEditingController();
+
+class Panel extends StatelessWidget {
+  final ScrollController controller;
+  final PanelController panelController;
+  const Panel({Key? key, required this.controller, required this.panelController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //bool favouritedToggle = false;
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+      controller: controller,
+      children: <Widget>[
+        const SizedBox(height: 25),
+        Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.camera_alt),
+              tooltip: 'Scan Nutritional Value',
+              onPressed: () {
+                print('Volume button clicked');
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.import_contacts),
+              tooltip: 'Import Favourite Item',
+              onPressed: () {
+                print('Volume button clicked');
+              },
+            ),
+            const Expanded(
+              child: Text(''),
+            ),
+            IconButton(
+              icon:  const Icon(Icons.favorite_outline),
+              tooltip: 'Favourite Item',
+              onPressed: () {
+                print('Volume button clicked');
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.close),
+              tooltip: 'Close Panel',
+              onPressed: () {
+                panelController.close();
+              },
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextField(
+            controller: foodName,
+            decoration: const InputDecoration(helperText: 'Food Name'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextField(
+            controller: calories,
+            decoration: const InputDecoration(helperText: 'Calories'),
+          ),
+        ),
+        const Center(
+          child: Text("Macro Calories(measured in grams)"),
+        ),
+        Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: protien,
+                decoration: const InputDecoration(helperText: "Est. Protein"),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Flexible(
+              child: TextField(
+                controller: fats,
+                decoration: const InputDecoration(helperText: "Est. Fats"),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Flexible(
+              child: TextField(
+                controller: carbs,
+                decoration: const InputDecoration(helperText: "Est. Carbs"),
+              ),
+            ),
+          ],
+        ),
+        Column(children: [
+          const SizedBox(height: 150),
+          FloatingActionButton(
+            // When the user presses the button, show an alert dialog containing
+            // the text that the user has entered into the text field.
+            onPressed: () {
+              print(foodName.text);
+              print(calories.text);
+              print(fats.text);
+              print(carbs.text);
+              print(protien.text);
+            },
+            tooltip: 'Show me the value!',
+            child: const Icon(Icons.plus_one),
+          ),
+        ]),
+      ],
+    );
+  }
+}
