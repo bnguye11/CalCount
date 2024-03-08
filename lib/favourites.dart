9f4403912e64ebfcbe36cba4ff1f264938ce46f4
BrianNguyen1099260 <xBrianNguyenn@gmail.com>
2024-03-08 14:32:35 -0500

    App/Drawer added

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
+import 'package:calcount/main.dart';
+import 'package:calcount/settings.dart';
+
 class Favourites extends StatelessWidget{
   const Favourites({super.key});
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Favourites"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Cal Counter'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Favourites'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Favourites()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                );
+              },
+            ),
+          ],
+        )
+      ),
     );
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
+import 'package:calcount/main.dart';
+import 'package:calcount/favourites.dart';
+
 class Settings extends StatelessWidget{
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Profile"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Cal Counter'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage(title: "Cal Counter")),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Favourites'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Favourites()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                );
+              },
+            ),
+          ],
+        )
+      ),
     );
   }
 }
